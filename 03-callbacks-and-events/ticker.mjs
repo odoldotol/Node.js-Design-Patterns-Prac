925df59859691f5eaa475263e8381343e90b74d7
odoldotol <odoldotol@gmail.com>
2023-07-05 23:45:01 +0900

    3.2

---
+++ 03-callbacks-and-events/ticker.mjs
@@ -0,0 +1,31 @@
+import { EventEmitter } from 'events';
+
+/**
+ * 
+ * @param {number} number 
+ * @param {(n:number)=>any} callback 
+ */
+function ticker(number, callback) {
+  const emitter = new EventEmitter();
+  let tick_count = 0;
+
+  setTimeout(() => callback(tick_count), number);
+
+  const ticker50 = () => {
+    setTimeout(() => {
+      emitter.emit('tick');
+      tick_count++;
+      ticker50();
+    }, 50);
+  };
+
+  ticker50();
+
+  return emitter;
+}
+
+const runningTicker = ticker(1000, (tick_count) => {
+  console.log(tick_count);
+});
+
+runningTicker.on('tick', () => console.log('tick'));
