acf008be43592c0a46e11e900ca271cc6e128095
odoldotol <odoldotol@gmail.com>
2023-07-10 10:46:29 +0900

    await study

--- 04-asynchronous-control-flow-patterns-with-callbacks/await.mjs
+++ 04-asynchronous-control-flow-patterns-with-callbacks/await.mjs
@@ -3,8 +3,8 @@ setTimeout(async () => {
   console.log("1");
   Promise.resolve("promise 2").then(console.log);
   setImmediate(() => console.log("setImmediate 3"));
-  console.log("4");
-  process.nextTick(() => console.log("nextTick 5"));
+  process.nextTick(() => console.log("nextTick 4"));
+  console.log("5");
   console.log("6");
 }, 0);
 
@@ -17,7 +17,7 @@ setTimeout(async () => {
   console.log("1");
   Promise.resolve("promise 2").then(console.log);
   setImmediate(() => console.log("setImmediate 3"));
-  console.log(await "await 4");
-  process.nextTick(() => console.log("nextTick 5"));
+  process.nextTick(() => console.log("nextTick 4"));
+  console.log(await "await 5");
   console.log("6");
 }, 200);
