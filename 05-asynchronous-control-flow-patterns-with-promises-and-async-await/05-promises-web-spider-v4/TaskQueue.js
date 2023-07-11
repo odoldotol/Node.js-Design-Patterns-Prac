cb68e736d2eaf7708781aa2f2d84f963cecf7ef2
odoldotol <odoldotol@gmail.com>
2023-07-11 21:23:13 +0900

    5.2

--- 05-asynchronous-control-flow-patterns-with-promises-and-async-await/05-promises-web-spider-v4/TaskQueue.js
+++ 05-asynchronous-control-flow-patterns-with-promises-and-async-await/05-promises-web-spider-v4/TaskQueue.js
@@ -5,23 +5,32 @@ export class TaskQueue {
     this.queue = []
   }
 
+  /**
+   * @param {() => Promise<any>} task 
+   */
   runTask (task) {
     return new Promise((resolve, reject) => {
-      this.queue.push(() => {
-        return task().then(resolve, reject)
+      this.queue.push(async () => {
+        try {
+          resolve(await task())
+        } catch (error) {
+          reject(error)
+        }
       })
       process.nextTick(this.next.bind(this))
     })
   }
 
-  next () {
+  async next () {
     while (this.running < this.concurrency && this.queue.length) {
       const task = this.queue.shift()
-      task().finally(() => {
-        this.running--
-        this.next()
-      })
       this.running++
+      try {
+        await task();
+      } finally {
+        this.running--
+        this.next();
+      }
     }
   }
 }
